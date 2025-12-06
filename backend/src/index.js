19ef78f608d3c6e5d66e05f2721df821d04a5fa0
Thunder2367shiv <2022kuec2034@iiitkota.ac.in>
2025-12-06 22:40:48 +0530

    chore: update CORS to allow vercel frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         "http://localhost:5173", 
-        "https://your-frontend-project.vercel.app"
+        "https://truestate-backend-neon.vercel.app"
     ],
     credentials: true
 }));
